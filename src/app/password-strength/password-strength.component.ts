bd02366607201dac707fb7cf6082c1d49b546d68
Ivan Korotenko <110201295+Vandey89@users.noreply.github.com>
2023-01-17 00:40:59 +0100

    red

--- src/app/password-strength/password-strength.component.ts
+++ src/app/password-strength/password-strength.component.ts
@@ -47,6 +47,7 @@ return force
 ngOnChanges(changes: { [propName: string]: SimpleChange }): void {
     const password = changes.passwordToCheck.currentValue;
     this.setBarColors(3, '#DDD');
+    (password < 1 ) ? this.setBarColors(3, '#DDD') : this.setBarColors(3, 'red') ;
     if (password) {
     const c = this.getColor(this.checkStrength(password));
     this.setBarColors(c.index, c.color);
